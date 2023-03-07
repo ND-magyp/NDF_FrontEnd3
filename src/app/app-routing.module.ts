03062085fc60eaf875b60eaa0f0bf8c8a9fa5004
ND-magyp <118853601+ND-magyp@users.noreply.github.com>
2023-03-07 19:27:04 -0300

    20230307

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './components/error404/error404.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/modals/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'login', component: LoginComponent},
+  {path: '**', component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
